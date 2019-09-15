dab4ba10011524b6ab26e1ada6c9359a0c9f2e14
Tilia <lindsey.snider@gmail.com>
2019-09-14 19:41:10 -0700

    saving some readme edits, and make_call is now a function

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 A simple app to make and receive phone calls. 
 
 [Link to the Twilio API Tutorial](https://www.twilio.com/docs/voice/quickstart/python)
+
+### Setup
+
+For the most part this follows the tutorial. You will need your own Twilio account and phone number.
+
+Unfinished
--- make_call.py
+++ make_call.py
@@ -1,16 +1,22 @@
 from twilio.rest import Client
 
-account_sid = open("account_sid.txt","r+")
 
-auth_token = open("auth_token.txt","r+")
+def make_call():
+    """a code to create a call using twilio API"""
+    account_sid = open("account_sid.txt","r+")
 
-client = Client(account_sid.read(),auth_token.read())
+    auth_token = open("auth_token.txt","r+")
 
-call = client.calls.create(
-    url='http://demo.twilio.com/docs/voice.xml',
-    to='+17733914873',
-    from_='+13126354221'
+    client = Client(account_sid.read(),auth_token.read())
 
-    )
+    call = client.calls.create(
+        url='http://demo.twilio.com/docs/voice.xml',
+        to='+17733914873',
+        from_='+13126354221'
 
-print(call.sid)
+        )
+
+    return call.sid
+
+if __name__ == "__main__":
+    make_call()
